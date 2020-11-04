1fdb2a0c5a94c98a590c4d94bbe86d1b8d472c7a
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-11-04 15:39:39 -0600

    feat(panes): Add gender pane layout & refactor

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -21,20 +21,15 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                       color: Color(0xff1d1e33),
-                      cardChild: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Icon(FontAwesomeIcons.mars, size: 80.0),
-                          SizedBox(height: 15.0),
-                          Text("MALE",
-                              style: TextStyle(
-                                  fontSize: 18.0, color: Color(0xff8D8E98))),
-                        ],
-                      )),
+                      cardChild: IconContent(
+                          icon: FontAwesomeIcons.mars, label: "MALE")),
                 ),
                 Expanded(
-                  child: ReusableCard(color: Color(0xff1d1e33)),
-                )
+                  child: ReusableCard(
+                      color: Color(0xff1d1e33),
+                      cardChild: IconContent(
+                          icon: FontAwesomeIcons.mars, label: "FEMALE")),
+                ),
               ],
             ),
           ),
@@ -51,6 +46,25 @@ class _InputPageState extends State<InputPage> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class IconContent extends StatelessWidget {
+  IconContent({@required this.icon, @required this.label});
+
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(icon, size: 80.0),
+        SizedBox(height: 15.0),
+        Text(label, style: TextStyle(fontSize: 18.0, color: Color(0xff8D8E98))),
+      ],
     );
   }
 }
