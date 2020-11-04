6072867191926789573dd7e914798d25f31f013b
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-11-04 15:28:13 -0600

    feat(common): Add pane basic

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "package:font_awesome_flutter/font_awesome_flutter.dart";
 
 class InputPage extends StatefulWidget {
   @override
@@ -18,7 +19,17 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: ReusableCard(color: Color(0xff1d1e33)),
+                  child: ReusableCard(
+                      color: Color(0xff1d1e33),
+                      cardChild: Column(
+                        children: <Widget>[
+                          Icon(FontAwesomeIcons.mars, size: 80.0),
+                          SizedBox(height: 15.0),
+                          Text("MALE",
+                              style: TextStyle(
+                                  fontSize: 18.0, color: Color(0xff8D8E98))),
+                        ],
+                      )),
                 ),
                 Expanded(
                   child: ReusableCard(color: Color(0xff1d1e33)),
@@ -44,11 +55,13 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.color});
+  ReusableCard({@required this.color, this.cardChild});
   final Color color;
+  final Widget cardChild;
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         color: color,
