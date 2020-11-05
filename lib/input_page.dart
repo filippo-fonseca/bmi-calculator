bee87fe62a605bd9ff612506a784fea2f27c7463
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-11-05 15:04:49 -0600

    feat(panes): Add color caching logic for upper panes

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import "package:font_awesome_flutter/font_awesome_flutter.dart";
 
-import 'icon_content.dart';
-import 'reusable_card.dart';
+import 'widgets/icon_content.dart';
+import 'widgets/reusable_card.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xff1d1e33);
+const inactiveCardColor = Color(0xff111328);
+const bottomContainerColor = Color(0xffeb1555);
 
 class InputPage extends StatefulWidget {
   @override
@@ -10,6 +15,26 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Color maleCardColor = inactiveCardColor;
+  Color femaleCardColor = inactiveCardColor;
+
+  void updateColor(int gender) {
+    // male card was pressed:
+    if (gender == 1) {
+      if (maleCardColor == inactiveCardColor) {
+        maleCardColor = activeCardColor;
+      } else {
+        maleCardColor = inactiveCardColor;
+      }
+    } else if (gender == 2) {
+      if (femaleCardColor == inactiveCardColor) {
+        femaleCardColor = activeCardColor;
+      } else {
+        femaleCardColor = inactiveCardColor;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,34 +49,50 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: GestureDetector(
                     onTap: () {
-                      print("The card has been pressed.");
+                      setState(() {
+                        updateColor(1);
+                      });
                     },
                     child: ReusableCard(
-                        color: Color(0xff1d1e33),
+                        color: maleCardColor,
                         cardChild: IconContent(
                             icon: FontAwesomeIcons.mars, label: "MALE")),
                   ),
                 ),
                 Expanded(
-                  child: ReusableCard(
-                      color: Color(0xff1d1e33),
-                      cardChild: IconContent(
-                          icon: FontAwesomeIcons.venus, label: "FEMALE")),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateColor(2);
+                      });
+                    },
+                    child: ReusableCard(
+                        color: femaleCardColor,
+                        cardChild: IconContent(
+                            icon: FontAwesomeIcons.venus, label: "FEMALE")),
+                  ),
                 ),
               ],
             ),
           ),
           Expanded(
-            child: ReusableCard(color: Color(0xff1d1e33)),
+            child: ReusableCard(color: activeCardColor),
           ),
           Expanded(
             child: Row(
               children: [
-                Expanded(child: ReusableCard(color: Color(0xff1d1e33))),
+                Expanded(child: ReusableCard(color: activeCardColor)),
                 Expanded(child: ReusableCard(color: Color(0xff1d1e33))),
               ],
             ),
           ),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.all(10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
+            child: Text("Calculate"),
+          )
         ],
       ),
     );
