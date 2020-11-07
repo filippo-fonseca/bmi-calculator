fee366ff6ead34b79ce25d9fcf18d4646abce49b
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-11-07 16:45:47 -0600

    ui results

--- lib/constants.dart
+++ lib/constants.dart
@@ -11,3 +11,12 @@ const kinactiveCardColor = Color(0xff111328);
 const kbottomContainerColor = Color(0xffeb1555);
 
 const kNumberStyle = TextStyle(fontSize: 50.0, fontWeight: FontWeight.w900);
+
+const kTitleTextStyle = TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold);
+
+const kresultTextstyle = TextStyle(
+    color: Color(0xff24d876), fontSize: 22.0, fontWeight: FontWeight.bold);
+
+const kBMITextStytle = TextStyle(fontSize: 100.0, fontWeight: FontWeight.bold);
+
+const kbodyTextStyle = TextStyle(fontSize: 22.0);
--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
 
+import "constants.dart";
+import 'widgets/reusable_card.dart';
+
 class ResultsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("BMI CALCULATOR"),
-        ),
-        body: Text("Hello"));
+      appBar: AppBar(
+        title: Text("BMI CALCULATOR"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child:
+                Container(child: Text("Your result", style: kTitleTextStyle)),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              color: kactiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("Normal", style: kresultTextstyle),
+                  Text("18.3", style: kBMITextStytle),
+                  Text("Your BMI result is quite low. you should eat more!",
+                      textAlign: TextAlign.center, style: kbodyTextStyle),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
