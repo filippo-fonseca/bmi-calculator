2087e6254ff96f60719d916fb78982a454c6e485
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-11-07 18:06:43 -0600

    fix(brain): Fix 18.5 sign

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -16,7 +16,7 @@ class CalculatorBrain {
   String getResult() {
     if (_bmi >= 25) {
       return 'Overweight';
-    } else if (_bmi > 18.5) {
+    } else if (_bmi >= 18.5) {
       return 'Normal';
     } else {
       return 'Underweight';
